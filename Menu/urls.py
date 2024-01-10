1671b4d31ff654946e89ea600ea8891b5e43809d
Etma117 <polllfuentes@gmail.com>
2024-01-10 16:35:20 -0600

    InstruccionesDEBUG

--- Menu/urls.py
+++ Menu/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import MenuListar, ProductoCrearView
+from .views import MenuListar, ProductoCrearView, ProductoEditarView, ProductoEliminarView, ProductoDetalle
 
 urlpatterns = [
     path('',MenuListar.as_view() , name= 'Menu'),
-    path('crearproducto/',ProductoCrearView.as_view(), name='CrearProducto'),
+    path('crearproducto/',ProductoCrearView.as_view(), name='CrearProducto'),    
+    path('editarproducto/<int:pk>/',ProductoEditarView.as_view(), name='EditarProducto'),
+    path('eliminarproducto/<int:pk>/',ProductoEliminarView.as_view(), name='EliminarProducto'),    
+    path('producto_detalles/<int:pk>/',ProductoDetalle.as_view(), name='DetalleProducto'),
 
 ]
--- Menu/views.py
+++ Menu/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
 
 from .models import Producto
 from .forms import ProductoForm
@@ -16,7 +17,27 @@ class ProductoCrearView(CreateView):
     template_name = 'crear_producto.html'
     context_object_name = 'Producto'
     form_class = ProductoForm
+    success_url = reverse_lazy('Menu')
     
     def form_valid(self, form):
         messages.success(self.request, 'El objeto se ha Creado exitosamente.')
         return super().form_valid(form)
+    
+class ProductoEditarView(UpdateView):
+    model = Producto
+    template_name = 'crear_producto.html'
+    form_class = ProductoForm
+    success_url = reverse_lazy('Menu')
+    success_message = 'Producto actualizado correctamente...'
+    
+
+class ProductoEliminarView(DeleteView):
+    model = Producto
+    template_name = 'eliminar_producto.html'
+    success_url = reverse_lazy('Menu')
+
+class ProductoDetalle(DetailView):
+    model = Producto
+    template_name = 'producto_detalle.html'
+    context_object_name = 'producto'  # Nombre de la variable en la plantilla
+    pk_url_kwarg = 'producto_id'  # Nombre del parámetro en la URL
