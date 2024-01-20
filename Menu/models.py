77a65c211d62e7b8e8fd712cdc5740a723ad203f
Etma117 <polllfuentes@gmail.com>
2024-01-19 23:33:39 -0600

    Correcion JSON, VistasCEE

--- Menu/models.py
+++ Menu/models.py
@@ -8,12 +8,19 @@ class CategoriaMenu(models.Model):
         return self.nombreCate
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
-    sabores = models.JSONField(blank=True, null=True)    
+    sabores = models.JSONField(blank=True, null=True, max_length=255)    
     precio = models.DecimalField(max_digits=10, decimal_places=2)   
     categoria = models.ForeignKey(CategoriaMenu, on_delete=models.CASCADE)
     imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
 
     def __str__(self):
         return self.nombre    
+    
+    def save(self, *args, **kwargs):
+        # Si sabores es una cadena, divídela por comas y almacénala como una lista
+        if isinstance(self.sabores, str):
+            self.sabores = [sabor.strip() for sabor in self.sabores.split(',')]
+        print (self.sabores)
+        super().save(*args, **kwargs)
--- Menu/views.py
+++ Menu/views.py
@@ -20,24 +20,34 @@ class ProductoCrearView(CreateView):
     success_url = reverse_lazy('Menu')
     
     def form_valid(self, form):
-        messages.success(self.request, 'El objeto se ha Creado exitosamente.')
+        messages.success(self.request, 'El platillo se ha creado exitosamente.')
         return super().form_valid(form)
     
 class ProductoEditarView(UpdateView):
     model = Producto
-    template_name = 'crear_producto.html'
+    template_name = 'editar_producto.html'
+    context_object_name = 'Producto'
     form_class = ProductoForm
     success_url = reverse_lazy('Menu')
-    success_message = 'Producto actualizado correctamente...'
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'El platillo se ha editado exitosamente.')
+        return super().form_valid(form)
     
 
 class ProductoEliminarView(DeleteView):
     model = Producto
     template_name = 'eliminar_producto.html'
+    context_object_name = 'Producto'
     success_url = reverse_lazy('Menu')
+
+    def form_valid(self, form):
+        messages.error(self.request, 'El platillo se ha dado de baja del menú.')
+        return super().form_valid(form)
+
 
 class ProductoDetalle(DetailView):
     model = Producto
     template_name = 'producto_detalle.html'
-    context_object_name = 'producto'  # Nombre de la variable en la plantilla
+    context_object_name = 'Producto'  # Nombre de la variable en la plantilla
     pk_url_kwarg = 'producto_id'  # Nombre del parámetro en la URL
