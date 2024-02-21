5b57843d5a07c4fa8703bd2d88ba0a473e68ff67
Dany248118 <ouen_daniel@hotmail.com>
2024-02-21 10:20:25 -0600

    Creo que funciona xD

--- Comanda/urls.py
+++ Comanda/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from .views import AgregarAlCarritoView
 urlpatterns = [
     path('', views.home, name= 'Comanda'),
     path('domicilio/', views.domicilio, name='Domicilio'),
+    path('agregar-al-carrito/<int:producto_id>/', AgregarAlCarritoView.as_view(), name='agregar_al_carrito'),
 ]
--- Comanda/views.py
+++ Comanda/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Carrito
+from Menu.models import Producto
 
 # Create your views here.
 def home(request):
@@ -6,3 +10,14 @@ def home(request):
 
 def domicilio(request):
     return render(request, 'domicilio.html')
+
+class AgregarAlCarritoView(LoginRequiredMixin, View):
+    login_url = 'login'  
+    redirect_field_name = 'next' 
+
+    def get(self, request, producto_id):
+        producto = Producto.objects.get(pk=producto_id)
+        carrito, created = Carrito.objects.get_or_create(usuario=request.user, producto=producto)
+        carrito.cantidad += 1
+        carrito.save()
+        return redirect('carrito')
