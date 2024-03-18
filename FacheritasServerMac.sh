1bb2a54fa67894dcb96d2809659e46e43eecd370
エスマ117 <polllfuentes@gmail.com>
2024-03-18 16:50:05 -0600

    Script MacOS e Instrucciones AutoMator

--- FacheritasServerMac.sh
+++ FacheritasServerMac.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+# 1- Abrir automator de MacOS
+# 2- Selecionar aplicacion
+# 3- en el segundo buscador casi en medio de todo, buscar "Ejecutar script shell"
+# 4- Pegar este codigo
+# 5- En la esquina superior izquierda, darle nombre y guardarlo en el escritorio
+# 6- Ejecutar la app que se generó en el escritorio
 
-source ToolCraftEnv/bin/activate
-
-python -c "import sys; print(sys.executable)"
-
-pip install -r requirements.txt >/dev/null 2>&1
-
-python manage.py runserver >/dev/null 2>&1 &
+cd ~/Desktop/FacherasCoffee
+if [ ! -f "FacheritasVenv/bin/activate" ]; then
+    python3 -m venv FacheritasVenv
+	echo "entorno creado"
+fi
+source FacheritasVenv/bin/activate
+echo "Entorno Activado"
+pip3 install -r requirements.txt > /dev/null
+echo "Requisitos instalados"
+python3 manage.py runserver > /dev/null 2>&1 &
+echo "Corriendo servidor..."
 
 sleep 5
-
 open http://127.0.0.1:8000
